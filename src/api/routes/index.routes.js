3d981d9cc73732717843a25c5068b3464bebe8b6
vergaraldvm <leonardo11vergara11@gmail.com>
2023-01-12 17:33:26 -0500

    billing info page advanced

--- src/api/routes/index.routes.js
+++ src/api/routes/index.routes.js
@@ -4,19 +4,23 @@ import hour from './hour.routes.js';
 import week from './week.routes.js';
 import preference from './preference.routes.js';
 import preferredHour from './preferredHour.routes.js';
-import { hello, login, logout } from '../controllers/index.controllers.js';
+import { hello, login, logout, billing, rating, usd } from '../controllers/index.controllers.js';
 
 const router = Router();
 
 router.get('/', hello);
 router.post('/login', login);
 router.post('/logout', logout);
+router.get('/billing', billing);
+router.get('/rating', rating);
+router.get('/usd', usd);
 
 router.use('/user', user);
 router.use('/hours', hour);
 router.use('/week', week);
 router.use('/preferences', preference);
 router.use('/preferredHours', preferredHour);
+
 
 router.use((_, res) => {
 	res.status(404).json({ message: 'Not found' });
--- src/constants.js
+++ src/constants.js
@@ -8,5 +8,7 @@ export const DB_PATH = path.join(__dirname, '../', 'db', 'db.sqlite3');
 export const SCHEDULE_INTERVAL = 'scheduleInterval';
 export const SCHEDULE_TIMEOUT = 'scheduleTimeout';
 
-export const URL_SETTINGS = 'https://www.tutor.com/providers/ic/settings';
 export const URL_SCHEDULE = 'https://www.tutor.com/providers/schedule';
+export const URL_BILLING = 'https://prv.tutor.com/nGEN/Apps/SocWinSupportingPages/Provider/BillingInfo.aspx?ProgramGUID=b611858b-4d02-4afe-8053-d082bbc1c58e';
+export const URL_RATING = 'https://prv.tutor.com/nGEN/Apps/SocWinSupportingPages/Provider/ConnectionInfo.aspx?ProgramGUID=b611858b-4d02-4afe-8053-d082bbc1c58e';
+export const URL_USD = 'https://www.google.com/finance/quote/USD-COP';
